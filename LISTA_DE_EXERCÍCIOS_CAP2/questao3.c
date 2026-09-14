d5eb1741d5971f116c2843e393a13cb46d150176
andressasouzadev <andressasouzadev@gmail.com>
2026-09-13 21:33:56 -0300

    Add number format display in questao3.c

--- LISTA_DE_EXERCÍCIOS_CAP2/questao3.c
+++ LISTA_DE_EXERCÍCIOS_CAP2/questao3.c
@@ -1,1 +1,17 @@
+#include <stdio.h>
+
+int main() {
+int numero;
+
+```
+printf("Digite um numero inteiro: ");
+scanf("%d", &numero);
+
+printf("Decimal: %d | Hexadecimal: %x | Octal: %o | ASCII: %c\n",
+       numero, numero, numero, numero);
+
+return 0;
+```
+
+}
 
